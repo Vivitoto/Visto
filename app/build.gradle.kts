a88c87f409edb4ed4b036d2151afaf85a0183d65
Vivitoto <1097429376@qq.com>
2026-06-23 08:55:55 +0000

    release: bump to v1.1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "app.visto"
         minSdk = 26
         targetSdk = 35
-        versionCode = 24
-        versionName = "1.1.3"
+        versionCode = 25
+        versionName = "1.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
