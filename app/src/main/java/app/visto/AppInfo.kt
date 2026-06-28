3fc0f261b5daef0f1ab1923537bf7ddd62d8001f
Vivitoto <78074573+Vivitoto@users.noreply.github.com>
2026-06-28 11:12:58 +0800

    fix: correct AppInfo version to 1.1.15

--- app/src/main/java/app/visto/AppInfo.kt
+++ app/src/main/java/app/visto/AppInfo.kt
@@ -3,6 +3,6 @@ package app.visto
 object AppInfo {
     const val APP_NAME = "Visto"
     const val PACKAGE_NAME = "app.visto"
-    const val VERSION_NAME = "1.1.14"
-    const val VERSION_CODE = 35
+    const val VERSION_NAME = "1.1.15"
+    const val VERSION_CODE = 36
 }
