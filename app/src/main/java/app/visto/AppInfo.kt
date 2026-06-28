a63b606da13872607f293f47547f46935705f94a
Vivitoto <78074573+Vivitoto@users.noreply.github.com>
2026-06-28 06:41:52 +0000

    fix: sync AppInfo to 1.1.19

--- app/src/main/java/app/visto/AppInfo.kt
+++ app/src/main/java/app/visto/AppInfo.kt
@@ -3,6 +3,6 @@ package app.visto
 object AppInfo {
     const val APP_NAME = "Visto"
     const val PACKAGE_NAME = "app.visto"
-    const val VERSION_NAME = "1.1.18"
-    const val VERSION_CODE = 39
+    const val VERSION_NAME = "1.1.19"
+    const val VERSION_CODE = 40
 }
